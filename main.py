ba97f6f581a977436db014f3681d941591580be5
Yan Rodrigues <yan.rodrigues.azevedo@hotmail.com>
2021-03-26 22:01:50 -0300

    API validando arquivos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import werkzeug
+from check_doc_content import verificar_documento
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,8 +16,8 @@ class HelloWorld(Resource):
         parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
         args = parse.parse_args()
         file = args['file']
-        file.save("first_file.jpg")
-        return {"data": "Posted"}
+        file.save(file.filename)
+        return verificar_documento(file.filename)
 
 
 api.add_resource(HelloWorld, "/helloworld")
